665ab3f6c6601fa48da579747e95e0a9750f472e
akshajsadwl-dotcom <akshajsadwl@gmail.com>
2026-06-23 17:31:36 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,8 @@
+// @ts-check
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: true,
-
-  allowedDevOrigins: [
-    "192.168.31.33",
-    "h8tr2v0j-3000.inc1.devtunnels.ms",
-  ],
-};
-
-export default nextConfig;
+  /* config options here */
+}
+ 
+module.exports = nextConfig
